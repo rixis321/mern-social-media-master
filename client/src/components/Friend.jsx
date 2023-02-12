79ebe9c0c36bae77cd950d74c6f2794f61dd29e4
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-02-12 21:00:53 +0100

    After changes

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -20,6 +20,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath,tools,services  }) =
   const medium = palette.neutral.medium;
 
   const isFriend = friends.find((friend) => friend._id === friendId);
+  const isSameUser = friendId === _id;
 
   const patchFriend = async () => {
     const response = await fetch(
@@ -64,16 +65,19 @@ const Friend = ({ friendId, name, subtitle, userPicturePath,tools,services  }) =
           </Typography>
         </Box>
       </FlexBetween>
-      <IconButton
-        onClick={() => patchFriend()}
-        sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
-      >
-        {isFriend ? (
-          <PersonRemoveOutlined sx={{ color: primaryDark }} />
-        ) : (
-          <PersonAddOutlined sx={{ color: primaryDark }} />
-        )}
-      </IconButton>
+        {!isSameUser ? (
+            <IconButton
+                onClick={() => patchFriend()}
+                sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+            >
+                {isFriend ? (
+                    <PersonRemoveOutlined sx={{ color: primaryDark }} />
+                ) : (
+                    <PersonAddOutlined sx={{ color: primaryDark }} />
+                )}
+            </IconButton>
+        ):''}
+
     </FlexBetween>
   );
 };
--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -65,7 +65,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
             comments={comments}
           />
         )
-      )}
+      ).reverse()}
     </>
   );
 };
